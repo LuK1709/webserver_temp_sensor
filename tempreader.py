e1f9d6ccda488a387cd38f6aeefeda71ba255f2a
Luca Kurz <luca.kurz@qnips.io>
2023-05-03 11:11:48 +0200

    Add proper cooling functionality

--- tempreader.py
+++ tempreader.py
@@ -5,44 +5,31 @@ import RPi.GPIO as GPIO
 #endPoint = "http://localhost/raumtemps"
 endPoint = "http://172.20.125.200:8080/raumtemps"
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.OUT)
+motor = GPIO.PWM(21, 50)
+
 def get_request(url):
         r = requests.get(url)
         result = r.json()
         return result
 
 def start_Motor():
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-
-        GPIO.setup(21, GPIO.OUT)
-        #GPIO.setup(25, GPIO.OUT)
-
-        uhrzeigersinn = GPIO.PWM(21, 50)
-        #gegen_uhrzeigersinn = GPIO.PWM(25, 50)
-
-        
-        #gegen_uhrzeigersinn.start(0)
-        uhrzeigersinn.start(0)
-        uhrzeigersinn.ChangeDutyCycle(100)
-        #gegen_uhrzeigersinn.ChangeDutyCycle(100)
-        # gegen_uhrzeigersinn.stop()
+        motor.start(0)
+        motor.ChangeDutyCycle(100)
 
 def stop_motor():
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+        motor.ChangeDutyCycle(0)
+        motor.stop()
 
-        GPIO.setup(21, GPIO.OUT)
-        #GPIO.setup(25, GPIO.OUT)
-
-        uhrzeigersinn = GPIO.PWM(21, 50)
-        uhrzeigersinn.stop()
-
-if __name__ == "__main__":        
+if __name__ == "__main__":                
         while True:
                 result = get_request(url = endPoint)
                 temp = result[0][0]
                 temp2 = temp
-                if float(temp) > 23:
+                print(temp)
+                if float(temp) > 24:
                         start_Motor()
                 else:
                         stop_motor()
